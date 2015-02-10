81b9fd646f685aa5b2e45470332b9cd6e7a1f7b5
Chris Bird <ckbird@ucdavis.edu>
2015-02-10 03:39:07 -0800

    Added Example Source/Header
    Purely for speed sake.

---
+++ EXAMPLE HEADER.h
@@ -0,0 +1,17 @@
+//TestClass.h
+#ifndef TEST_CLASS_H
+#define TEST_CLASS_H
+
+class testClass
+{
+public:
+	testClass();
+	~testClass();
+
+private:
+
+protected:
+
+};
+
+#endif
---
+++ EXAMPLESOURCE.cpp
@@ -0,0 +1,19 @@
+//TestClass.c
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "iostream"
+#include "stdlib.h"
+#include "math.h"
+
+#include "EXAMPLE HEADER.h"
+
+testClass::testClass()
+{
+	//Constructor
+};
+
+testClass::~testClass()
+{
+	//Destructor
+};
